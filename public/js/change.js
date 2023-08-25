cb8e793eab3b071520df8fa8749053c4e6cb45aa
KingSanMon <98154605+KingSanMon@users.noreply.github.com>
2023-08-25 23:27:56 +0300

    upgrade

--- public/js/change.js
+++ public/js/change.js
@@ -1,6 +1,4 @@
-var block = $("<div>").addClass("new-center").text("Итого:");
-
-$(".btn-me").click(function(){
+$("#btn-me").click(function(){
     // Вызывает код при нажатии на кнопку отправить себе
 
     $("#top").css("display", "none");
@@ -13,11 +11,41 @@ $(".btn-me").click(function(){
 
     $("#btn").text("Вернуться назад");
 
-    $(".center").addClass("center-upd");
+    $("#btn").addClass("back");
 
     $("#center").css("margin", "0");
 
-    $("#btn-block").css("padding", "2.31rem 0rem 0rem 0rem");
+    $("#btn-block").css("padding", "1.80rem 2.89rem 0rem");
 
-    $(".center-upd").after(block);
+    $("#center").after("<div class='new-center'><p>Итого: <span id='result'>1000</span></p><div class='email'><input type='email' placeholder='Введите ваш email'>");
+
+    $(".bottom").css("margin-top", "1.44rem")
+
+    var result = parseInt($("#result").text());
+    var number = parseInt($("#number").text());
+
+    function ymno(a, b){
+        return(a*b);
+    }
+
+    $("#result").text(ymno(result, number));
+
+    $("#plus").click(function(){
+        var newNumber = ++number;
+
+        $("#result").text(ymno(result, newNumber))
+    });
+
+    $(".back").click(function(){
+        // Кнопка вернутся 
+
+        $("#top").css("display", "flex");
+        $("#btn-block").css("padding", "0");
+        $("#center").css("margin", "2.58rem 0 2.59rem 0");
+        
+        $("#btn-me").text("Отправить себе");
+        $("#btn").text("Отправить в подарок");
+
+        $(".new-center").remove()
+    });
 });
