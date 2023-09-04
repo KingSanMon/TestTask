67d803999bd34c2af7ad00c9dde39085d9cb4f05
KingSanMon <cfif3550555@gmail.com>
2023-09-04 19:55:54 +0300

    comments_code

--- public/js/letter.js
+++ public/js/letter.js
@@ -1,15 +1,15 @@
-$(document).on("click","#btn", function(){
+// $(document).on("click","#btn", function(){
 
-    if ($(this).attr("id") === "btn"){
-         // сносим две секции
-        $("#work").css("display", "none");
-        $("#certificate").css("display", "none");
+//     if ($(this).attr("id") === "btn"){
+//          // сносим две секции
+//         $("#work").css("display", "none");
+//         $("#certificate").css("display", "none");
 
-        // изменяем кнопки
-        $("#btn-me").text("Отправить и оплатить");
-        $("#btn").text("Вернуться назад");
+//         // изменяем кнопки
+//         $("#btn-me").text("Отправить и оплатить");
+//         $("#btn").text("Вернуться назад");
 
-        // добавляем новый блок
-        $("#letter").css("display", "block");
-    }
-});
+//         // добавляем новый блок
+//         $("#letter").css("display", "block");
+//     }
+// });
