469b9c7be93a3e41f4a8d15e37de0452b3796667
KingSanMon <cfif3550555@gmail.com>
2023-09-13 21:41:55 +0300

    update adiptiv

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+const navbar = document.querySelector("#navbar");
+const navbutton = document.querySelector("#nav-button");
+const navImg = document.querySelector("#button-open");
+
+navbutton.onclick = () => {
+    if (navbar.classList.toggle('open')) {
+        navImg.src = "public/image/close.png";
+    } else {
+        navImg.src = "public/image/open.png";
+    }
+}
